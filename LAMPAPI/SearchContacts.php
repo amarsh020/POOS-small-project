368ed43b09240522b66711ba8d9f8e7805a15b2f
Raudy Brito <raudyb02@gmail.com>
2025-01-22 02:57:41 -0500

    Search Contacts now returns objects

--- LAMPAPI/SearchContacts.php
+++ LAMPAPI/SearchContacts.php
@@ -1,58 +1,59 @@
-
 <?php
 
-	$inData = getRequestInfo();
-	
-	$id = 0;
-	$firstName = "";
-	$lastName = "";
+$inData = getRequestInfo();
 
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "contactsWebsite"); 	
-	if( $conn->connect_error )
-	{
-		returnWithError( $conn->connect_error );
-	}
-	else
-	{
-		$stmt = $conn->prepare("SELECT ID,firstName,lastName FROM Users WHERE Login=? AND Password =?");
-		$stmt->bind_param("ss", $inData["login"], $inData["password"]);
-		$stmt->execute();
-		$result = $stmt->get_result();
+$searchResults = "";
+$searchCount = 0;
 
-		if( $row = $result->fetch_assoc()  )
-		{
-			returnWithInfo( $row['firstName'], $row['lastName'], $row['ID'] );
+$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "contactsWebsite");
+if ($conn->connect_error) {
+	returnWithError($conn->connect_error);
+} else {
+	$stmt = $conn->prepare("select * from Contacts where FirstName like ? and UserID=?");
+	$Name = "%" . $inData["search"] . "%";
+	$stmt->bind_param("ss", $Name, $inData["userId"]);
+	$stmt->execute();
+
+	$result = $stmt->get_result();
+
+	while ($row = $result->fetch_assoc()) {
+		if ($searchCount > 0) {
+			$searchResults .= ",";
 		}
-		else
-		{
-			returnWithError("No Records Found");
-		}
+		$searchCount++;
 
-		$stmt->close();
-		$conn->close();
-	}
-	
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
+		$searchResults .= '{"FirstName" : "' . $row["FirstName"] . '", "LastName" : "' . $row["LastName"] . '", "Phone" : "' . $row["Phone"] . '", "Email" : "' . $row["Email"] . '"}';
 	}
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
+	if ($searchCount == 0) {
+		returnWithError("No Records Found");
+	} else {
+		returnWithInfo($searchResults);
 	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
-	function returnWithInfo( $firstName, $lastName, $id )
-	{
-		$retValue = '{"id":' . $id . ',"firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
-?>
+
+	$stmt->close();
+	$conn->close();
+}
+
+function getRequestInfo()
+{
+	return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson($obj)
+{
+	header('Content-type: application/json');
+	echo $obj;
+}
+
+function returnWithError($err)
+{
+	$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+	sendResultInfoAsJson($retValue);
+}
+
+function returnWithInfo($searchResults)
+{
+	$retValue = '{"results":[' . $searchResults . '],"error":""}';
+	sendResultInfoAsJson($retValue);
+}
